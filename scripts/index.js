13240b3a553bbb6e1b25326de8a10d7683505a51
Dmitry Bulykin <dbulyk@yandex.ru>
2021-10-25 00:13:34 +0300

    Начата логика добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,9 @@ const profileDesc = document.querySelector('.profile-info__description');
 const formElement = document.querySelector('.form');
 const inputProfileName = document.querySelector('.form__item_el_heading');
 const inputProfileDesc = document.querySelector('.form__item_el_desc');
+const elements = document.querySelector('.elements__list');
+const inputNamePic = document.querySelector('.form__item_el_namePic');
+const inputUrlPic = document.querySelector('.form__item_el_url');
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -41,6 +44,30 @@ popup.addEventListener('click', function() {
   }
 })
 
+function addPic(inputNamePic, inputUrlPic) {
+  const element = document.createElement('li');
+  element.classList.add('element');
+  const elementPicture = document.createElement('img');
+  elementPicture.classList.add('element__photo');
+  elementPicture.src = inputUrlPic;
+  elementPicture.alt = "Фото"
+  const elementDescription = document.createElement('div');
+  elementDescription.classList.add('element__description');
+  const elementTitle = document.createElement('p');
+  elementTitle.classList.add('element__title');
+  elementTitle.textContent = inputNamePic;
+  const elementLikeBtn = document.createElement('button');
+  elementLikeBtn.classList.add('element__like btn');
+  elementLikeBtn.type = 'button';
+  elements.insertAdjacentHTML('afterbegin', `
+  <li class="element">
+    <img src="${inputUrlPic}" alt="Фото" class="element__photo">
+    <div class="element__description">
+      <p class="element__title">${inputNamePic}</p>
+      <button type="button" class="element__like btn"></button>
+    </div>
+  </li>
+  `)}
 
 
 
